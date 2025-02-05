e112e19b046697480bd8b37790f0f6bb4e293a13
Noah Smith <noah.smith@stu.ocsb.ca>
2025-02-05 15:52:52 +0000

    Created HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+// Says hello to Ben
+
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello Ben!");
+        System.out.println("How is school going?");
+    }
+}
